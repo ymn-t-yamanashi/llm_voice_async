245a0a4f549e932a42d0d317e27b9a9e5ac650b1
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2025-12-08 08:12:18 +0900

    first commit

---
+++ lib/llm_async_web/live/index.ex
@@ -0,0 +1,71 @@
+defmodule LlmAsyncWeb.Index do
+  use LlmAsyncWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    socket =
+      assign(socket, text: "実行ボタンを押してください")
+      |> assign(input_text: "Elixirについて教えてください")
+      |> assign(btn: true)
+
+    {:ok, socket}
+  end
+
+  def handle_event("start", _, socket) do
+    pid = self()
+    input_text = socket.assigns.input_text
+
+    socket =
+      assign(socket, btn: false)
+      |> assign(text: "")
+      |> assign_async(:ret, fn -> run(pid, input_text) end)
+
+    {:noreply, socket}
+  end
+
+  def handle_event("update_text", %{"text" => new_text}, socket) do
+    {:noreply, assign(socket, input_text: new_text)}
+  end
+
+  def handle_info(%{"done" => false, "response" => response}, socket) do
+    text = socket.assigns.text <> response
+    {:noreply, assign(socket, text: text)}
+  end
+
+  def handle_info(%{"done" => true}, socket) do
+    socket =
+      assign(socket, btn: true)
+
+    {:noreply, socket}
+  end
+
+  def run(pid, text) do
+    client = Ollama.init()
+
+    {:ok, stream} =
+      Ollama.completion(client,
+        model: "gemma3:27b",
+        prompt: text,
+        stream: true
+      )
+
+    stream
+    |> Stream.each(&Process.send(pid, &1, []))
+    |> Stream.run()
+
+    {:ok, %{ret: :ok}}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <Layouts.app flash={@flash}>
+      <div class="p-5">
+        <form>
+          <textarea id="text_input" name="text" phx-change="update_text" class="input w-[400px]">{@input_text}</textarea>
+        </form>
+        <button disabled={!@btn} class="btn" phx-click="start">実行</button>
+        <p class="m-2">{@text}</p>
+      </div>
+    </Layouts.app>
+    """
+  end
+end
